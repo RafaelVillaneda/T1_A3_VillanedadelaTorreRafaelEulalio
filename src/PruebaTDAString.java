6a0b23e59dcc3f62fdf89c96ec8394b80da79ee4
Rafael Eulalio Villaneda de la torre <Rafael.rafaeua2401@outlook.com>
2020-09-19 12:12:51 -0500

    Se termino el punto 3 y se completo el menu principal

--- src/PruebaTDAString.java
+++ src/PruebaTDAString.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -48,14 +49,53 @@ class StringTDA{
 			int poci=Integer.parseInt(subcadEliminar.split("-")[0]),poci2=Integer.parseInt(subcadEliminar.split("-")[1]);
 			poci=poci-1;
 			poci2=poci2-1;
-			for(int i=0;i<getCadena().length();i++) {
-				if(!(i>=poci && i<=poci2)) {
-					cadRes=cadRes+getCadena().substring(i,i+1);
+			if((poci<getCadena().length() || poci2<getCadena().length()) ||(!(poci<0 || poci2<0))) {
+				for(int i=0;i<getCadena().length();i++) {
+					if(!(i>=poci && i<=poci2)) {
+						cadRes=cadRes+getCadena().substring(i,i+1);
+					}
 				}
+			}else {
+				System.out.println("Los limites que ingresaste no exiten");
 			}
 			setCadena(cadRes);
 			System.out.println("Resultado: "+getCadena());
 		}
+	}
+	public void mostrarMayusculaCadaPalabra() {
+		int num=0;
+		for(int i=0;i<getCadena().length();i++) {
+			if(getCadena().substring(i, i+1).equalsIgnoreCase(" ")) {
+				num++;
+			}
+		}//for
+		if(num>0) {
+			String palabras[]=getCadena().split(" ");
+			String cadNueva="";
+			for(int i=0;i<palabras.length;i++) {
+				for(int x=0;x<palabras[i].length();x++) {
+					if(x==0) {
+						cadNueva=cadNueva+palabras[i].substring(x, x+1).toUpperCase();
+					}else {
+						cadNueva=cadNueva+palabras[i].substring(x, x+1);
+					}
+				}
+				cadNueva=cadNueva+" ";
+				//cadNueva=cadNueva+palabras[i];
+			}
+			setCadena(cadNueva);
+		}else {
+			String cad="";
+			for(int i=0;i<getCadena().length();i++) {
+				if(i==0) {
+					cad=cad+getCadena().substring(i, i+1).toUpperCase();
+				}else {
+					cad=cad+getCadena().substring(i, i+1);
+				}
+			}
+			setCadena(cad);
+		}
+		System.out.println(getCadena());
 	}
 	
 }
@@ -90,25 +130,36 @@ public class PruebaTDAString {
 				boolean bandera=true;
 				String op1="";
 				do {
-				System.out.println("Elige la opcion deseada");
-				System.out.println("A) Eliminar 'X' caracter de la palabra");
-				System.out.println("B) Eliminar fragmento de la palabra en base a su pocicion ("+cade.getCadena().length()+" tamaño de espacios en palabra actual)");
-				op1=entrada.nextLine().toUpperCase();
-				switch (op1) {
-				case "A":
-					cade.agregarEliminarCaracteresY_OSubcadenasPociEspecificas(op1);
-					break;
-				case "B":
-					cade.agregarEliminarCaracteresY_OSubcadenasPociEspecificas(op1);
-					break;
-				default:
-					System.out.println("Elige una opcion correcta");
-					break;
-				}
-				if(op1.equalsIgnoreCase("A") || op1.equalsIgnoreCase("B")) {
-					bandera=false;
-					}
-				}while(bandera==true);
+					System.out.println("Elige la opcion deseada");
+					System.out.println("A) Eliminar 'X' caracter de la palabra");
+					System.out.println("B) Eliminar fragmento de la palabra en base a su pocicion ("+cade.getCadena().length()+" tamaño de espacios en palabra actual)");
+					op1=entrada.nextLine().toUpperCase();
+					switch (op1) {
+					case "A":
+						cade.agregarEliminarCaracteresY_OSubcadenasPociEspecificas(op1);
+						break;
+					case "B":
+						cade.agregarEliminarCaracteresY_OSubcadenasPociEspecificas(op1);
+						break;
+					default:
+						System.out.println("Elige una opcion correcta");
+						break;
+					}	
+					if(op1.equalsIgnoreCase("A") || op1.equalsIgnoreCase("B")) {
+						bandera=false;
+						}
+					}while(bandera==true);
+				break;
+			case 3:
+				cade.mostrarMayusculaCadaPalabra();
+				break;
+			case 4:
+				System.out.println("Ingresa la nueva palabra/Frase");
+				String nuevo=entrada.nextLine();
+				cade.setCadena(nuevo);
+				break;
+			case 5:
+				System.out.println("Saliendo");
 				break;
 			default:
 				System.out.println("Ingresa una opcion disponible que este en el menu");
